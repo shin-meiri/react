46001cc358d3e3413f1d5f4cc0fadb9bc5817974
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-12 00:38:23 +0700

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import MyCard from "./MyCard"; // Import komponen baru
 
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>Halo dari App.jsx!</h1>
+      <MyCard /> {/* Pakai komponen JSX-mu */}
     </div>
   );
 }
